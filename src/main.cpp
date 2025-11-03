ae99c4dd27b7d306678c4e417a4bbd4d54fd1527
Jacob G <71324202+KGratkiewicz@users.noreply.github.com>
2025-11-03 15:21:58 +0000

    zadnie 2-1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 //Jakub Grątkiewicz
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Zadanie 2
+// Zadanie 2.1
+string z2_1_path_symbole_txt = "../zalaczniki-2025/symbole.txt";
+
+bool czy_palindrom(string tekst){
+    int ile_znakow = tekst.length();
+    for(int i =0; i < ile_znakow/2 ; i++){
+        if(tekst[i] != tekst[ile_znakow-i-1])
+            return false;
+    }
+    return true;
+}
+
+void Zadanie2_1(){
+    ifstream plik(z2_1_path_symbole_txt);
+    if(!plik.is_open()){
+        cerr << "Nie mozna otworzyc pliku" << endl;
+    }
+
+    string linia;
+    while(plik >> linia){
+        if(czy_palindrom(linia)){
+            cout << linia << endl;
+        }
+    }
+    plik.close();
+}
+
 int main() {
-    std::cout << "Hello, Matura 2025!\n";
+    Zadanie2_1();
     return 0;
 }
